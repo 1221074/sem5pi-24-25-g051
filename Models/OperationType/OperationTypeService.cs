fea977a0b0df44aed65602722c4eaf72b2cb9cb2
Goncalo Ribeiro <1220702@isep.ipp.pt>
2024-10-18 18:35:21 +0100

    creating the Operation Type Service

---
+++ Models/OperationType/OperationTypeService.cs
@@ -0,0 +1,109 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using sem5pi_24_25_g051.Models.Shared;
+
+namespace sem5pi_24_25_g051.Models.OperationType {
+    public class OperationTypeService {
+
+        private readonly IOperationTypeRepository _OTrepo;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OperationTypeService(IOperationTypeRepository OTrepo, IUnitOfWork unitOfWork) {
+            _OTrepo = OTrepo;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<OperationTypeDTO>> GetAllAsync() {
+            var list = await this._OTrepo.GetAllAsync();
+
+            List<OperationTypeDTO> listDTO = list.ConvertAll(OT => new OperationTypeDTO {
+                Id = OT.Id.AsGuid(),
+                Name = OT.Name,
+                Description = OT.Description
+            });
+
+            return listDTO;
+        }
+        
+        public async Task<OperationTypeDTO> GetByIdAsync(OperationTypeId id) {
+            var OT = await this._OTrepo.GetByIdAsync(id);
+
+            if (OT == null) {
+                return null;
+            }
+
+            return new OperationTypeDTO {
+                Id = OT.Id.AsGuid(),
+                Description = OT.Description
+            };
+        }
+
+        public async Task<OperationTypeDTO> AddAsync(CreatingOperationTypeDTO OTDTO) {
+            var OT = new OperationType(OTDTO.Name, OTDTO.Description);
+
+            await this._OTrepo.AddAsync(OT);
+            await this._unitOfWork.CommitAsync();
+
+            return new OperationTypeDTO {
+                Id = OT.Id.AsGuid(),
+                Description = OT.Description
+            };
+        }
+
+        public async Task<OperationTypeDTO> UpdateAsync(OperationTypeDTO OTDTO) {
+            var OT = await this._OTrepo.GetByIdAsync(new OperationTypeId(OTDTO.Id));
+
+            if (OT == null) {
+                return null;
+            }
+
+            OT.ChangeDescription(OTDTO.Description);
+
+            await this._unitOfWork.CommitAsync();
+
+            return new OperationTypeDTO {
+                Id = OT.Id.AsGuid(),
+                Description = OT.Description
+            };
+        }
+
+        public async Task<OperationTypeDTO> InactivateAsync(OperationTypeId id) {
+            var OT = await this._OTrepo.GetByIdAsync(id);
+
+            if (OT == null) {
+                return null;
+            }
+
+            OT.MarkAsInative();
+
+            await this._unitOfWork.CommitAsync();
+
+            return new OperationTypeDTO {
+                Id = OT.Id.AsGuid(),
+                Description = OT.Description
+            };
+        }
+
+        public async Task<OperationTypeDTO> DeleteAsync(OperationTypeId id) {
+            var OT = await this._OTrepo.GetByIdAsync(id);
+
+            if (OT == null) {
+                return null;
+            }
+
+            if (OT.Active) {
+                throw new BusinessRuleValidationException("It is not possible to delete an active operation type.");
+            }
+
+            this._OTrepo.Remove(OT);
+            await this._unitOfWork.CommitAsync();
+
+            return new OperationTypeDTO {
+                Id = OT.Id.AsGuid(),
+                Description = OT.Description
+            };
+
+        }
+    }
+
+}
