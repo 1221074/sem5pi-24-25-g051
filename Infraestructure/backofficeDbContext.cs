723440edf270e2897d2191690e9fa619d3e34582
Guilherme Pinto <1221074@isep.ipp.pt>
2024-10-19 13:50:59 +0100

    error update in dbContext

--- Infraestructure/backofficeDbContext.cs
+++ Infraestructure/backofficeDbContext.cs
@@ -6,6 +6,7 @@ using sem5pi_24_25_g051.Models.SurgeryRoom;
 using sem5pi_24_25_g051.Models.OperationRequest;
 using sem5pi_24_25_g051.Models.OperationType;
 using sem5pi_24_25_g051.Infraestructure.OperationTypes;
+using sem5pi_24_25_g051.Models.User;
 
 
 namespace sem5pi_24_25_g051.Infraestructure
@@ -28,7 +29,7 @@ namespace sem5pi_24_25_g051.Infraestructure
         public DbSet<Appointment> Appointment { get; set; } = default!;
         public DbSet<Staff> Staff { get; set; } = default!;
         public DbSet<SurgeryRoom> SurgeryRoom { get; set; } = default!;
-        //public DbSet<User> User { get; set; } = default!;
+        public DbSet<User> Users { get; set; } = default!;
         public DbSet<Patient> Patients { get; set; }
         public DbSet<OperationRequest> OperationRequest { get; set; } = default!;
         public DbSet<OperationType> OperationType { get; set; } = default!;
