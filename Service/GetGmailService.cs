105d80832700d9190ea9d79c5420d60b98851571
Guilherme Pinto <1221074@isep.ipp.pt>
2024-10-25 22:09:08 +0100

    User service mail change #1 #2

---
+++ Service/GetGmailService.cs
@@ -0,0 +1,72 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using MimeKit;
+using System.IO;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace sem5pi_24_25_g051.Services{
+
+
+
+    public class GetGmailService {
+
+
+    private static GmailService GetMailService()
+    { 
+        UserCredential credential;
+   string credentialPath = @"Service\credentials.json";
+ using (var stream = new FileStream(credentialPath, FileMode.Open, FileAccess.Read))
+    {
+        string tokenPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "token.json");
+        credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.Load(stream).Secrets,
+            new[] { GmailService.Scope.GmailSend },
+            "user", CancellationToken.None,
+            new FileDataStore(tokenPath, true)).Result;
+    }
+
+    // Create Gmail API service.
+    var service = new GmailService(new BaseClientService.Initializer()
+    {
+        HttpClientInitializer = credential,
+        ApplicationName = "sem5pi-24-25-g051",
+    });
+
+    return service;
+    }
+    public async Task SendEmailUsingGmailApi(string toEmail, string subject, string body) {
+        var service = GetMailService();
+
+        // Create the email using MimeKit
+        var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress("lapr5", "sem5pi2425-g051@gmail.com"));
+        emailMessage.To.Add(new MailboxAddress("", toEmail));
+        emailMessage.Subject = subject;
+        emailMessage.Body = new TextPart("plain") { Text = body };
+
+        // Encode the email to Base64
+        using (var stream = new MemoryStream()) {
+            emailMessage.WriteTo(stream);
+            var rawMessage = Convert.ToBase64String(stream.ToArray())
+            .Replace('+', '-')
+            .Replace('/', '_')
+            .Replace("=", "");
+
+            var message = new Message
+            {
+                Raw = rawMessage
+            };
+
+                // Send the email
+                await service.Users.Messages.Send(message, "me").ExecuteAsync();
+            }
+
+        }
+    }
+}
