439f1f386f7409078c9d3a14affec3c08b2790e7
Guilherme Pinto <1221074@isep.ipp.pt>
2024-11-04 10:27:04 +0000

    angular app dev

--- 3D Visualization Module/angular-three/src/app/app.component.ts
+++ 3D Visualization Module/angular-three/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-three';
+  values: string[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getValues().subscribe(
+      (data) => (this.values = data),
+      (error) => console.error(error)
+    );
+}
 }
