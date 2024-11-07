aec154ba5dcd78388287f76f2752d64f4a12a133
Goncalo Ribeiro <1220702@isep.ipp.pt>
2024-11-07 09:05:21 +0000

    adding THREE.js demo project

--- 3DVisualizationModule/angular-three/src/app/app.routes.ts
+++ 3DVisualizationModule/angular-three/src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { SpecializationComponent } from './component/specialization/specialization.component'
 import { DetailsComponent } from './component/details/details.component';
+import { CubeComponent } from './cube/cube.component';
 
 const routeConfig: Routes = [
-  {path:'', component: SpecializationComponent,
-  title:'Home Page'},
-   {
-    path: 'details/:name',
-    component: DetailsComponent,
-    title: 'Details Page'
-   }
+  
+  { path:'', component: SpecializationComponent, title:'Home Page' },
+  { path: 'details/:name', component: DetailsComponent, title: 'Details Page' },
+
+  // Redirect to the cube component on app load
+  // { path: '', redirectTo: '/cube', pathMatch: 'full' },
+  // Route for the cube component
+  { path: 'cube', component: CubeComponent },
 ];
 
 export  default routeConfig;
