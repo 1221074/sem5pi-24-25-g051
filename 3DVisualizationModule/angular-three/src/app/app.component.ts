ebe3df4ff8d41d7101074071bfd0eb22ce8fc9e7
Gonçalo <1220897@isep.ipp.pt>
2024-11-08 19:49:04 +0000

    Changed admin interface to bue user friendly

--- 3DVisualizationModule/angular-three/src/app/app.component.ts
+++ 3DVisualizationModule/angular-three/src/app/app.component.ts
@@ -12,14 +12,14 @@ import { AdminComponent } from './component/admin/admin.component';
   standalone: true,
   imports: [RouterModule,CommonModule,LoginComponent,CubeComponent,SpecializationComponent,HomePageComponent, AdminComponent, CommonModule],
   template: `
-  <main>
+  
      <header class="brand-name">
        <h1>Surgical Appointment and Resource Management</h1>
       </header>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
-    </main>
+  
   `,
   styleUrls: ['./app.component.scss'],
 
