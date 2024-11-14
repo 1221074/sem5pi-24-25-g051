b58bdce774ee4008f87cd7c79f205f7bcb8cc052
Gonçalo <1220897@isep.ipp.pt>
2024-11-14 20:54:16 +0000

    #62 #63 #64 added the first objects to the reception room and toilets

--- 3DVisualizationModule/angular-three/src/app/hospital/Basic_Thumb_Raiser/maze.js
+++ 3DVisualizationModule/angular-three/src/app/hospital/Basic_Thumb_Raiser/maze.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import Ground from "./ground.js";
 import Wall from "./wall.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 /*
  * parameters = {
@@ -100,6 +101,154 @@ export default class Maze {
             // onError callback
             error => this.onError(this.url, error)
         );
+
+        // Create a resource .gltf or .glb file loader
+        const loaderGLTF = new GLTFLoader();
+
+        // CHAIRS
+        loaderGLTF.load('./models/tandem_seating_-_hospital/scene.gltf', (gltf) => {
+            this.chair = gltf.scene;
+            this.chair.scale.set(0.4, 0.4, 0.4);
+            this.chair.position.set(-9.85, 0, -3);
+            this.chair.rotateY(3 * Math.PI / 1.975); //1.94 virado para a porta
+            this.object.add(this.chair);
+
+            let chair2 = gltf.scene.clone();
+            chair2.position.set(-9.85, 0, 3);
+            this.object.add(chair2);
+
+            let chair3 = gltf.scene.clone();
+            chair3.position.set(-9.85, 0, 2);
+            this.object.add(chair3);
+
+            let chair4 = gltf.scene.clone();
+            chair4.position.set(-9.85, 0, -2);
+            this.object.add(chair4);
+
+            //=======================================================================================================
+            // waiting room 1
+            let chair5 = gltf.scene.clone();
+            chair5.position.set(-1.6, 0, -5.35);
+            chair5.rotateY(3 * Math.PI / 2);
+            this.object.add(chair5);
+            
+            let chair6 = gltf.scene.clone();
+            chair6.position.set(-0.4, 0, -5.35);
+            chair6.rotateY(3 * Math.PI / 2);
+            this.object.add(chair6);
+
+            let chair7 = gltf.scene.clone();
+            chair7.position.set(-1.6, 0, -4.6);
+            chair7.rotateY( 3 * Math.PI / 2);
+            this.object.add(chair7);
+
+            let chair8 = gltf.scene.clone();
+            chair8.position.set(-0.4, 0, -4.6);
+            chair8.rotateY( 3 * Math.PI / 2);
+            this.object.add(chair8);
+        
+            let chair9 = gltf.scene.clone();
+            chair9.position.set(-1.6, 0, -3.85);
+            chair9.rotateY( 3 * Math.PI / 2);
+            this.object.add(chair9);
+        
+            let chair10 = gltf.scene.clone();
+            chair10.position.set(-0.4, 0, -3.85);
+            chair10.rotateY( 3 * Math.PI / 2);
+            this.object.add(chair10);
+        
+
+            //=======================================================================================================
+            // waiting room 2
+            let chair11 = gltf.scene.clone();
+            chair11.position.set(-1.6, 0, 5.35);
+            chair11.rotateY(Math.PI / 2);
+            this.object.add(chair11);
+            
+            let chair12 = gltf.scene.clone();
+            chair12.position.set(-0.4, 0, 5.35);
+            chair12.rotateY(Math.PI / 2);
+            this.object.add(chair12);
+
+            let chair13 = gltf.scene.clone();
+            chair13.position.set(-1.6, 0, 4.6);
+            chair13.rotateY(Math.PI / 2);
+            this.object.add(chair13);
+
+            let chair14 = gltf.scene.clone();
+            chair14.position.set(-0.4, 0, 4.6);
+            chair14.rotateY(Math.PI / 2);
+            this.object.add(chair14);
+        
+            let chair15 = gltf.scene.clone();
+            chair15.position.set(-1.6, 0, 3.85);
+            chair15.rotateY(Math.PI / 2);
+            this.object.add(chair15);
+        
+            let chair16 = gltf.scene.clone();
+            chair16.position.set(-0.4, 0, 3.85);
+            chair16.rotateY(Math.PI / 2);
+            this.object.add(chair16);
+        } 
+        );
+
+        //=======================================================================================================
+        // Reception desk
+        loaderGLTF.load('./models/reception_desk/scene.gltf', (gltf) => {
+            this.desk = gltf.scene;
+            this.desk.scale.set(4, 5.5, 4);
+            this.desk.position.set(-8.72, 0, -0.67);
+            this.desk.rotateY(3 * Math.PI / 2);
+            this.object.add(this.desk);
+        }
+        );
+
+        //=======================================================================================================
+        // Reception pc
+        loaderGLTF.load('./models/imac_computer/scene.gltf', (gltf) => {
+            this.pc = gltf.scene;
+            this.pc.scale.set(0.37, 0.5, 0.37);
+            this.pc.position.set(-8.55, 0.4, 0);
+            this.pc.rotateY(3 * Math.PI / 2);
+            this.object.add(this.pc);
+        }
+        );
+
+        //=======================================================================================================
+        // Toilet 
+        loaderGLTF.load('./models/toilet/scene.gltf', (gltf) => {
+            this.toilet = gltf.scene;
+            this.toilet.scale.set(0.4, 0.4, 0.4);
+            this.toilet.position.set(-8.2, 0.2, -5.35);
+            this.toilet.rotateY(3 * Math.PI / 2);
+            this.object.add(this.toilet);
+
+            let toilet2 = gltf.scene.clone();
+            toilet2.position.set(-8.2, 0.2, 5.35);
+            toilet2.rotateY(Math.PI);
+            this.object.add(toilet2);
+
+            let toilet3 = gltf.scene.clone();
+            toilet3.position.set(6.55, 0.2, -5.35);
+            this.object.add(toilet3);
+
+            let toilet4 = gltf.scene.clone();
+            toilet4.position.set(6.55, 0.2, 5.35);
+            toilet4.rotateY(Math.PI);
+            this.object.add(toilet4);
+        }
+        );
+
+
+        // KOENIGSEGG ONE PRO
+        /*loaderGLTF.load('./models/koenigsegg_one_pro/scene.gltf', (gltf) => {
+            this.chair = gltf.scene;
+            this.chair.scale.set(10, 10, 10);
+            this.chair.position.set(0, 0, 0);
+            this.chair.rotateY(3 * Math.PI / 1.975); //1.94 virado para a porta
+            this.object.add(this.chair);
+        }
+        );*/
     }
 
     // Convert cell [row, column] coordinates to cartesian (x, y, z) coordinates
--- 3DVisualizationModule/angular-three/src/app/hospital/Basic_Thumb_Raiser/normal_door.js
+++ 3DVisualizationModule/angular-three/src/app/hospital/Basic_Thumb_Raiser/normal_door.js
@@ -18,6 +18,117 @@ export default class NormalDoor {
 
         const frameSize = { width: 0.840, height: 1.788, depth: 0.045 };
         const doorSize = { width: 0.654, height: 1.686, depth: 0.035, gap: 0.0465 };
+        let scene, camera, renderer, controls;
 
+        // Create a material
+        const sideMaterial = new THREE.MeshBasicMaterial({ color: 0xc36e2d });
+
+        // Create the frame
+
+        // Create a box geometry
+        let geometry = new THREE.BoxGeometry(frameSize.width, frameSize.height, frameSize.depth);
+
+        // Create a texture
+        let texture = new THREE.TextureLoader().load("./textures/frame_front.png");
+        texture.colorSpace = THREE.SRGBColorSpace;
+
+        // Create a material
+        let frontMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture });
+        frontMaterial.transparent = true;
+
+        // Create a texture
+        texture = new THREE.TextureLoader().load("./textures/frame_back.png");
+        texture.colorSpace = THREE.SRGBColorSpace;
+
+        // Create a material
+        let backMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture });
+        backMaterial.transparent = true;
+
+        // Create a mesh with the specified geometry and materials
+        let mesh = new THREE.Mesh(geometry, [sideMaterial, sideMaterial, sideMaterial, sideMaterial, frontMaterial, backMaterial]);
+
+        // Add the mesh to the scene
+        scene.add(mesh);
+
+        // Create the door
+
+        // Create a box geometry
+        geometry = new THREE.BoxGeometry(doorSize.width, doorSize.height, doorSize.depth);
+
+        // Create a texture
+        texture = new THREE.TextureLoader().load("./textures/door_front.png");
+        texture.colorSpace = THREE.SRGBColorSpace;
+
+        // Create a material
+        frontMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture });
+
+        // Create a texture
+        texture = new THREE.TextureLoader().load("./textures/door_back.png");
+        texture.colorSpace = THREE.SRGBColorSpace;
+
+        // Create a material
+        backMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture });
+
+        // Create a mesh with the specified geometry and materials
+        mesh = new THREE.Mesh(geometry, [sideMaterial, sideMaterial, sideMaterial, sideMaterial, frontMaterial, backMaterial]);
+        mesh.translateX(doorSize.width / 2.0);
+        mesh.translateY(-doorSize.gap);
+
+        // Create a group
+
+        const door = new THREE.Group();
+
+        // Add the mesh to the group
+        door.add(mesh);
+        door.translateX(-doorSize.width / 2.0);
+
+        // Add the group to the scene
+        scene.add(door);
+
+        // Create and configure two tweens
+        let state = "close";
+        const tween = new TWEEN.Tween(door.rotation);
+
+        // Create and configure the GUI
+        const gui = new GUI();
+        const actions = {
+            open: () => {
+                if (state != "open") {
+                    state = "open";
+                    tween.stop();
+                    tween.to({ y: Math.PI / 2.0 }, 2000 * (1.0 - door.rotation.y / (Math.PI / 2.0)));
+                    tween.startFromCurrentValues();
+                }
+            },
+            stop: () => {
+                state = "stop";
+                tween.stop();
+            },
+            close: () => {
+                if (state != "close") {
+                    state = "close";
+                    tween.stop();
+                    tween.to({ y: 0.0 }, 2000 * door.rotation.y / (Math.PI / 2.0));
+                    tween.startFromCurrentValues();
+                }
+            }
+        };
+        gui.add(actions, "open");
+        gui.add(actions, "stop");
+        gui.add(actions, "close");
+
+        // Set the camera position;
+        camera.position.z = 2.5;
+
+        // Register the event handler to be called on window resize
+        window.addEventListener("resize", windowResize);
+
+        animate();
+    }
+
+    windowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
     }
 }
