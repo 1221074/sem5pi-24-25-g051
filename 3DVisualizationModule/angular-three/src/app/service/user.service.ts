ea37dd013faf42c6b5cee61c80d547a8fef2f564
Guilherme Pinto <1221074@isep.ipp.pt>
2024-11-15 13:30:18 +0000

    User creation service in frontend

---
+++ 3DVisualizationModule/angular-three/src/app/service/user.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../interface/user';
+import { response } from 'express';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private url = 'https://localhost:7252/api/user';
+
+  constructor(){}
+
+  async getAllUsers(): Promise<User[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+  async getUserById(id: string): Promise<User | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? [];
+  }
+
+  async createUser(userData: any) {
+    try {
+      const response = await fetch(this.url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+        credentials: 'include',
+      });
+
+      if (!response.ok) {
+        // Handle non-200 responses
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Error posting user');
+      }
+    }catch(error){
+      console.error('Error:', error);
+      // Handle or rethrow the error as appropriate
+      throw error;
+    }
+  }
+
+
+}
